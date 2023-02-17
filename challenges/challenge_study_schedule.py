75649405529f6b37f270ec5095cd536782dfb72d
alfeukramer <alfeukramer@gmail.com>
2023-02-17 16:14:48 -0300

    feat: implementa contador - req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+
+    for first_item, last_item in permanence_period:
+        print(first_item, last_item)
+        try:
+            if first_item <= target_time <= last_item:
+                counter += 1
+        except TypeError:
+            return None
+
+    return counter
