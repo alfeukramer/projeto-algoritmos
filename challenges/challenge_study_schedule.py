b057035152ba21d1db4f79f8bac6dab9497d8c41
alfeukramer <alfeukramer@gmail.com>
2023-02-17 16:31:15 -0300

    style: remove print

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,7 +2,6 @@ def study_schedule(permanence_period, target_time):
     counter = 0
 
     for first_item, last_item in permanence_period:
-        print(first_item, last_item)
         try:
             if first_item <= target_time <= last_item:
                 counter += 1
