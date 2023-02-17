f22e8833cf1a2c678bf6b1e68addd0ea6d4e1773
alfeukramer <alfeukramer@gmail.com>
2023-02-17 18:33:11 -0300

    implementa teste encrypt message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+
+
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(3, 3)
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(3, "a")
+
+    assert encrypt_message("😷🤒🤕🤢🤮🤧", 3) == "🤕🤒😷_🤧🤮🤢"
+
+    assert encrypt_message("😷🤒🤕🤢🤮🤧", 4) == "🤧🤮_🤢🤕🤒😷"
+
+    assert encrypt_message("😷🤒🤕🤢🤮🤧", 9) == "🤧🤮🤢🤕🤒😷"
